84c055fc685be13b8bcb63ce6444ad5d699543d0
Xavier Doxie <xdoxie@umd.edu>
2020-12-13 17:37:56 -0500

    commit

---
+++ exercise5/starterCode/TweetBot.js
@@ -0,0 +1,10 @@
+var Twitter = require('twitter');
+const express = require('express');
+const server = express();
+
+var client = new Twitter({
+    consumer_key: 'h3J8WrezTJdOey5JIawSPvWcF',
+    consumer_secret: 'Tr9StCCsi1T6UYpMM4p8lIlsD6wwjP9fuq1olonOSTFWooa7nq',
+    access_token_key: '3316204298-0AmXjHJzmuV8neyWsFEJgoouCQMiZaOfIqLNEA4',
+    access_token_secret: 'faOJF8CYaVBregD0Z339nZ7GZ2sPUOleNmPTUpq5Jo43X'
+});
